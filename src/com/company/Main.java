134af4f60af063116b726a11a0451c31afc35221
johnBrowning9 <j.t.browning9@gmail.com>
2021-10-15 14:35:54 -0500

    Initial setup stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        JFrame frame = new JFrame("City Sign Inventory Management");
+        frame.setContentPane(new mainForm().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ src/com/company/mainForm.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import javax.swing.*;
+
+public class mainForm {
+    // Initialize
+    public JPanel panel1;
+
+
+
+    // Constructor
+    public mainForm() {
+        panel1 = new JPanel();
+
+
+
+    }
+}
