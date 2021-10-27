ada0e6502acf54c471a228232462600ef5dc7d55
Claire Winogrodzki <clairewinogrodzki@MacBookPro13s-MacBook-Pro.local>
2021-10-27 16:06:29 -0500

    <Claire_test>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,16 @@
 package com.company;
 
 import javax.swing.*;
-import java.sql.SQLException;
 
 public class Main {
 
-    public static DataBase dataBase;
-
     public static void main(String[] args) {
-
-        // Create instance of Database
-        try {
-            dataBase = new DataBase();
-        } catch (SQLException e){
-            JOptionPane.showMessageDialog(null,"Error creating DB: " + e.getMessage(),"ERROR!", 1);
-        }
-        /////////////////////////////////////
-
+	// write your code here
         JFrame frame = new JFrame("City Sign Inventory Management");
-        //frame.setContentPane(new mainForm().panel1);
-        frame.setSize(500,500);
-        frame.setContentPane(new mainForm());
+        frame.setContentPane(new mainForm().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.pack();
+        frame.pack();
         frame.setVisible(true);
-
     }
+    // testing
 }
