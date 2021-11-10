0e5d1399c49cded55b6d3f3c75dee858f15efac5
johnBrowning9 <j.t.browning9@gmail.com>
2021-11-10 16:43:43 -0600

    added the Invoice class

---
+++ src/com/company/Invoice.java
@@ -0,0 +1,45 @@
+package com.company;
+import java.util.ArrayList;
+
+public class Invoice {
+    private float price;
+    private Sign sign;
+    private ArrayList<String> pickupDates;
+
+    public Invoice(){
+        sign = null;
+        price = 0;
+        pickupDates = new ArrayList<>();
+    }
+
+
+    // ACCESSORS
+
+    public ArrayList<String> getPickupDates() {
+        return pickupDates;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public Sign getSign() {
+        return sign;
+    }
+
+
+    // MUTATORS
+
+
+    public void setSign(Sign sign) {
+        this.sign = sign;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setPickupDates(ArrayList<String> pickupDates) {
+        this.pickupDates = pickupDates;
+    }
+}
