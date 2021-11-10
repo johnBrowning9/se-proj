f0235669912d21598d9cf3d90c4fa23d4e2732f4
ninja <johyh>
2021-11-10 16:27:53 -0600

    Refactored the User class to more closely resemble the data in the database and added accessor methods.

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -3,16 +3,19 @@ import java.util.HashMap;
 import java.util.Enumeration;
 
 /**
- * This class represents a generic user. It includes fields for a username and password.
+ * This class represents a generic user. It includes fields for the user's name, id, role, and whether or not the user
+ * is active according to the database. This class also keeps track of the user's permissions through the permissions
+ * HashMap.
  */
 public class User {
     private String role; //Role of the user. Ex: customer, database admin, traffic manager, etc.
-    private String userName;
-    private String password;
+    private String name; //user's name as listed in the database
+    private int id; //user's id as listed in the database
+    private boolean isActive; //user's activity as listed in the database
     private HashMap<String, Boolean> permissions; //Represents the different permissions the user has based on their role.
 
     //Constructor for the user class
-    public User(String role, String userName, String password){
+    public User(String role, String name, int id, boolean isActive){
         permissions = new HashMap<>();
         this.role = role;
 
@@ -24,8 +27,9 @@ public class User {
             permissions.put("reserveSign", false);
         }
 
-        this.userName = userName;
-        this.password = password;
+        this.name = name;
+        this.id = id;
+        this.isActive = isActive;
     }
 
     /**
@@ -50,5 +54,41 @@ public class User {
             //user does not have permission to reserve signs
             System.out.println("You do not have permission to reserve this sign.");
         }
+    }
+
+
+    //Accessors
+
+
+    /**
+     * Accessor for the user's id.
+     * @return the user's id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Accessor for the user's name.
+     * @return the user's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Accessor for isActive.
+     * @return true if the user is active, false otherwise
+     */
+    public boolean getIsActive(){
+        return isActive;
+    }
+
+    /**
+     * Accessor for the user's role.
+     * @return the user's role
+     */
+    public String getRole(){
+        return role;
     }
 }
